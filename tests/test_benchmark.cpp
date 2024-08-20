2d1bdc23496d799563f8d453b83a34a3047214a5
Justin Shetty <jas0504@gmail.com>
2024-08-20 20:14:21 +0900

    update test to match example args

--- tests/test_benchmark.cpp
+++ tests/test_benchmark.cpp
@@ -4,12 +4,17 @@
 
 TEST(solve, benchmark)
 {
+  auto MT = std::mt19937(0);
+  const auto allow_following = true;
+  const auto verbosity = 0;
   const auto scen_filename = "./assets/random-32-32-10-random-1.scen";
   const auto map_filename = "./assets/random-32-32-10.map";
   const auto N = 50;
+
   const auto ins = Instance(scen_filename, map_filename, N);
-  const auto allow_following = true;
-  const auto verbosity = 2;
-  auto solution = solve(ins, verbosity, nullptr, nullptr, N, allow_following);
-  ASSERT_TRUE(is_feasible_solution(ins, solution, verbosity, N, allow_following));
+  ASSERT_TRUE(ins.is_valid(verbosity));
+
+  auto solution = solve(ins, verbosity, nullptr, &MT, N, allow_following);
+  ASSERT_TRUE(
+      is_feasible_solution(ins, solution, verbosity, N, allow_following));
 }
