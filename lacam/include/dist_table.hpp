87d03077be7acc8206bd2637f0bfc1bb93f3835f
Justin Shetty <jas0504@gmail.com>
2024-08-23 16:26:30 +0900

    cleanup DistTable and start DistTable2

--- lacam/include/dist_table.hpp
+++ lacam/include/dist_table.hpp
@@ -13,11 +13,43 @@ struct DistTable {
       table;  // distance table, index: agent-id & vertex-id
   std::vector<std::queue<Vertex*> > OPEN;  // search queue
 
-  int get(int i, int v_id);   // agent, vertex-id
-  int get(int i, Vertex* v);  // agent, vertex
+  int get(int i, int v_id);         // agent, vertex-id
+  inline int get(int i, Vertex* v)  // agent, vertex
+  {
+    return get(i, v->id);
+  }
 
-  DistTable(const Instance& ins);
   DistTable(const Instance* ins);
+  DistTable(const Instance& ins) : DistTable(&ins) {}
+
+  void setup(const Instance* ins);  // initialization
+};
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+struct DistTableMultiGoal {
+  const int K;  // number of vertices
+  std::vector<std::vector<int> >
+      table;  // distance table, index: agent-id & vertex-id
+  std::vector<std::queue<Vertex*> > OPEN;  // search queue
+
+  int get(int i, int v_id);         // agent, vertex-id
+  inline int get(int i, Vertex* v)  // agent, vertex
+  {
+    return get(i, v->id);
+  }
+
+  DistTableMultiGoal(const Instance* ins);
+  DistTableMultiGoal(const Instance& ins) : DistTableMultiGoal(&ins) {}
 
   void setup(const Instance* ins);  // initialization
 };
--- lacam/src/dist_table.cpp
+++ lacam/src/dist_table.cpp
@@ -1,11 +1,5 @@
 #include "../include/dist_table.hpp"
 
-DistTable::DistTable(const Instance& ins)
-    : K(ins.G.V.size()), table(ins.N, std::vector<int>(K, K))
-{
-  setup(&ins);
-}
-
 DistTable::DistTable(const Instance* ins)
     : K(ins->G.V.size()), table(ins->N, std::vector<int>(K, K))
 {
@@ -47,4 +41,54 @@ int DistTable::get(int i, int v_id)
   return K;
 }
 
-int DistTable::get(int i, Vertex* v) { return get(i, v->id); }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+DistTableMultiGoal::DistTableMultiGoal(const Instance* ins)
+    : K(ins->G.V.size()), table(ins->N, std::vector<int>(K, K))
+{
+  setup(ins);
+}
+
+void DistTableMultiGoal::setup(const Instance* ins)
+{
+  for (size_t i = 0; i < ins->N; ++i) {
+    OPEN.push_back(std::queue<Vertex*>());
+    auto n = ins->goals[i];
+    OPEN[i].push(n);
+    table[i][n->id] = 0;
+  }
+}
+
+int DistTableMultiGoal::get(int i, int v_id)
+{
+  if (table[i][v_id] < K) return table[i][v_id];
+
+  /*
+   * BFS with lazy evaluation
+   * c.f., Reverse Resumable A*
+   * https://www.aaai.org/Papers/AIIDE/2005/AIIDE05-020.pdf
+   */
+
+  while (!OPEN[i].empty()) {
+    auto n = OPEN[i].front();
+    OPEN[i].pop();
+    const int d_n = table[i][n->id];
+    for (auto& m : n->neighbor) {
+      const int d_m = table[i][m->id];
+      if (d_n + 1 >= d_m) continue;
+      table[i][m->id] = d_n + 1;
+      OPEN[i].push(m);
+    }
+    if (n->id == v_id) return d_n;
+  }
+  return K;
+}
